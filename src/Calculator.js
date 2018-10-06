bc6556c48efde0d7fd6a91db85064639a5aecb21
Bhargav Bachina <bhargavbachina@yahoo.com>
2018-10-06 09:56:41 -0500

    initila commit

---
+++ src/Calculator.js
@@ -0,0 +1,13 @@
+function Calculator() {}
+
+Calculator.prototype.add = function(a,b) {
+    return a+b;
+}
+
+Calculator.prototype.subtract = function(a,b) {
+    return a-b;
+}
+
+Calculator.prototype.multiply = function(a,b) {
+    return a*b;
+}
